8b5de6f693185baf271a2d7d8fccd12eaaadf930
Giampiero Baggiani <giampiero@sferalabs.cc>
2026-04-20 13:07:28 +0200

    Squashed 'commons/' content from commit ddba800
    git-subtree-dir: commons
    git-subtree-split: ddba8009a5b115806dde67adbd57be9f03f06304

---
+++ scripts/kmod-common.mk
@@ -0,0 +1,54 @@
+SOURCE_DIR ?= $(if $(src),$(src),$(CURDIR))
+MODULE_NAME ?= $(strip $(shell cat $(SOURCE_DIR)/MODULE_NAME))
+MODULE_VERSION := $(strip $(shell cat $(SOURCE_DIR)/VERSION))
+ifeq ($(strip $(MODULE_VERSION_DEFINE)),)
+MODULE_VERSION_DEFINE := $(shell echo "$(MODULE_NAME)" | tr '[:lower:]-' '[:upper:]_')_MODULE_VERSION
+endif
+ifeq ($(strip $(DTS_NAME)),)
+DTS_NAME := $(MODULE_NAME)
+endif
+
+obj-m += $(MODULE_NAME).o
+$(MODULE_NAME)-objs := $(MODULE_MAIN_OBJ)
+$(foreach m,$(COMMON_MODULES),$(eval $(MODULE_NAME)-objs += commons/$(m)/$(m).o))
+$(MODULE_NAME)-objs += $(MODULE_EXTRA_OBJS)
+
+ccflags-y += -D$(MODULE_VERSION_DEFINE)=\"$(MODULE_VERSION)\"
+
+KVER ?= $(if $(KERNELRELEASE),$(KERNELRELEASE),$(shell uname -r))
+KDIR ?= /lib/modules/$(KVER)/build
+
+all: dtbo
+	make -C $(KDIR) M=$(PWD) modules
+
+dtbo: $(DTS_NAME).dts
+	dtc -@ -Hepapr -I dts -O dtb -o $(DTS_NAME).dtbo $(DTS_NAME).dts
+
+clean:
+	make -C $(KDIR) M=$(PWD) clean
+	rm -f $(DTS_NAME).dtbo
+
+install:
+	sudo install -D -m 644 -c $(MODULE_NAME).ko /lib/modules/$(KVER)/updates/dkms/$(MODULE_NAME).ko
+	sudo depmod
+	sudo $(MAKE) install-extra
+
+install-extra: dtbo
+	install -D -m 644 -c $(DTS_NAME).dtbo /boot/overlays/$(DTS_NAME).dtbo
+	@if [ -n "$(strip $(UDEV_RULES))" ]; then \
+		for rule in $(UDEV_RULES); do \
+			install -D -m 644 -c $$rule /etc/udev/rules.d/$$rule; \
+		done; \
+	fi
+	udevadm control --reload-rules || true
+	udevadm trigger || true
+
+uninstall-extra:
+	rm -f /boot/overlays/$(DTS_NAME).dtbo
+	@if [ -n "$(strip $(UDEV_RULES))" ]; then \
+		for rule in $(UDEV_RULES); do \
+			rm -f /etc/udev/rules.d/$$rule; \
+		done; \
+	fi
+	udevadm control --reload-rules || true
+	udevadm trigger || true
