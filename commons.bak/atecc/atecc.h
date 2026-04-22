8e19820d6a9292c6e1dd05135ea6e932ac1ddbeb
Giampiero Baggiani <giampiero@sferalabs.cc>
2026-04-22 18:58:25 +0200

    Remove commons.bak

--- commons.bak/atecc/atecc.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SL_ATECC_H
-#define _SL_ATECC_H
-
-#include <linux/device.h>
-
-ssize_t devAttrAteccSerial_show(struct device *dev,
-                                struct device_attribute *attr, char *buf);
-#endif
--- commons.bak/scripts/dkms-post-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-PROJECT_DIR="$(cd "$SCRIPT_DIR/../.." && pwd)"
-cd "$PROJECT_DIR"
-
-make install-extra
