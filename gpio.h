b58e04656778c66e842f0a719847f36fe6d04465
Giampiero Baggiani <giampiero@sferalabs.cc>
2026-04-22 18:59:39 +0200

    Squashed 'commons/gpio/' changes from b60b6a9..2dd0dc5
    2dd0dc5 gpio: fix GPIO descriptor lifetime handling
    c1dbe0d gpio: update
    
    git-subtree-dir: commons/gpio
    git-subtree-split: 2dd0dc5f6ec06a7cb4feab4d276d55f4c6284dbc

--- gpio.h
+++ gpio.h
@@ -1,32 +1,32 @@
 #ifndef _SL_GPIO_H
 #define _SL_GPIO_H
 
-#include <linux/version.h>
-#include <linux/platform_device.h>
 #include <linux/gpio/consumer.h>
+#include <linux/platform_device.h>
+#include <linux/version.h>
 
 #define DEBOUNCE_DEFAULT_TIME_USEC 50000ul
 #define DEBOUNCE_STATE_NOT_DEFINED -1
 
 struct GpioBean {
-	const char *name;
-	struct gpio_desc *desc;
-	enum gpiod_flags flags;
-	bool invert;
-	void *owner;
+  const char *name;
+  struct gpio_desc *desc;
+  enum gpiod_flags flags;
+  bool invert;
+  void *owner;
 };
 
 struct DebouncedGpioBean {
-	struct GpioBean gpio;
-	int value;
-	int irq;
-	bool irqRequested;
-	unsigned long onMinTime_usec;
-	unsigned long offMinTime_usec;
-	unsigned long onCnt;
-	unsigned long offCnt;
-	struct hrtimer timer;
-	struct kernfs_node *notifKn;
+  struct GpioBean gpio;
+  int value;
+  int irq;
+  bool irqRequested;
+  unsigned long onMinTime_usec;
+  unsigned long offMinTime_usec;
+  unsigned long onCnt;
+  unsigned long offCnt;
+  struct hrtimer timer;
+  struct kernfs_node *notifKn;
 };
 
 void gpioSetPlatformDev(struct platform_device *pdev);
@@ -44,41 +44,45 @@ int gpioGetVal(struct GpioBean *g);
 void gpioSetVal(struct GpioBean *g, int val);
 
 ssize_t devAttrGpioMode_show(struct device *dev, struct device_attribute *attr,
-		char *buf);
+                             char *buf);
 
 ssize_t devAttrGpioMode_store(struct device *dev, struct device_attribute *attr,
-		const char *buf, size_t count);
+                              const char *buf, size_t count);
 
 ssize_t devAttrGpio_show(struct device *dev, struct device_attribute *attr,
-		char *buf);
+                         char *buf);
 
 ssize_t devAttrGpio_store(struct device *dev, struct device_attribute *attr,
-		const char *buf, size_t count);
+                          const char *buf, size_t count);
 
 ssize_t devAttrGpioDeb_show(struct device *dev, struct device_attribute *attr,
-		char *buf);
+                            char *buf);
 
 ssize_t devAttrGpioDebMsOn_show(struct device *dev,
-		struct device_attribute *attr, char *buf);
+                                struct device_attribute *attr, char *buf);
 
 ssize_t devAttrGpioDebMsOn_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t count);
+                                 struct device_attribute *attr, const char *buf,
+                                 size_t count);
 
 ssize_t devAttrGpioDebMsOff_show(struct device *dev,
-		struct device_attribute *attr, char *buf);
+                                 struct device_attribute *attr, char *buf);
 
 ssize_t devAttrGpioDebMsOff_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t count);
+                                  struct device_attribute *attr,
+                                  const char *buf, size_t count);
 
 ssize_t devAttrGpioDebOnCnt_show(struct device *dev,
-		struct device_attribute *attr, char *buf);
+                                 struct device_attribute *attr, char *buf);
 
 ssize_t devAttrGpioDebOffCnt_show(struct device *dev,
-		struct device_attribute *attr, char *buf);
+                                  struct device_attribute *attr, char *buf);
 
 ssize_t devAttrGpioBlink_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t count);
+                               struct device_attribute *attr, const char *buf,
+                               size_t count);
 
-struct GpioBean* gpioGetBean(struct device *dev, struct device_attribute *attr);
+struct GpioBean *gpioGetBean(struct device *dev, struct device_attribute *attr,
+                             const char **vals);
 
 #endif
